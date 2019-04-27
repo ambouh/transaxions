c95a84c295416a2c378a637ef640608cf33d5781
ambouh <andresmbouh@gmail.com>
2019-04-27 14:30:04 -0400

    Updates

---
+++ precache-manifest.176254c38e931fc766338519a5995754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "157efa5273506d30b0d5b06cf1b9d78a",
+    "url": "/ambouh/transaxions.git/static/media/whitelogo.157efa52.svg"
+  },
+  {
+    "revision": "37b202bdb07def26c28e",
+    "url": "/ambouh/transaxions.git/static/js/runtime~main.37b202bd.js"
+  },
+  {
+    "revision": "a2328ac35d5f599bc8ab",
+    "url": "/ambouh/transaxions.git/static/js/main.a2328ac3.chunk.js"
+  },
+  {
+    "revision": "5981a105fe83a5f6e8aa",
+    "url": "/ambouh/transaxions.git/static/js/1.5981a105.chunk.js"
+  },
+  {
+    "revision": "a2328ac35d5f599bc8ab",
+    "url": "/ambouh/transaxions.git/static/css/main.dd245df0.chunk.css"
+  },
+  {
+    "revision": "c63eeebcc19afb8ed96b9ef721a91258",
+    "url": "/ambouh/transaxions.git/index.html"
+  }
+];
